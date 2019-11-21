769689906bb73d7449cfde5f60e0cd5eba95ac7a
Aluno <aluno@etec.sp.gov.br>
2019-11-21 15:19:08 -0200

    changes

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+
+// caso o usuário clique em sair
+if(isset($_REQUEST['sair'])) {
+
+   unset($_SESSION['gescolar_dados_usuario']) // destroi a sessão de  autenticação do usuário
+   header("location login.php"); // redireciona para a pagina de login
+}
+
+// protegendo a página contra o acesso sem autenticação
+if(!isset($_SESSION['gescolar_dados_usuario'])) {
+   header("location login.php"); // redireciona para o login.
+}
+
+// abreviando o nome da variável que contém os dados do usuário.
+$usuario = $_SESSION['gescolar_dados_usuario'];
+
+?>
 <!DOCTYPE html>
 <html>
    <head>
@@ -5,13 +23,10 @@
    </head>
    <body>
      <div id="global">
-          <h1>GESCOLAR</h1>
-          <nav>
-            <ul>
-               <li><a href="cadastro_alunos.php">Cadastrar Aluno</a></li>
-               <li><a href="cadastro_alunos.php">Cadastrar Aluno</a></li>
-           </ul>
-           </nav>
+          <!-- Exibindo o nome do usuário que está guardado na sessão, com outros dados -->
+          <h1> GESCOLAR <small>, Bem-vindo <?= $usuario['nome'] ?> </small> </h1>
+
+          <?php include_once 'includes/cabecalho.php' ?>
        </div>
    </body>
 </html>
